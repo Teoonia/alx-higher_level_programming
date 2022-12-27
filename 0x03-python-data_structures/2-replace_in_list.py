68b5bdfe7e0fac448aac944fd86058b7d7832914
Teoonia <olawooreayomikun@gmail.com>
2022-12-27 12:53:37 -0800

    .py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for idx in range(len(my_list):)
-        if idx == 0:
-            my_list[idx] = element
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
     return(my_list)
